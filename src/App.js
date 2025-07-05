5387fb6d47ad0c63bc5dd283b6445d4bf1da1b14
mustafa <mustafa.salah.2131@gmail.com>
2025-07-06 02:29:32 +0300

    Fetches and manages question data.
    Implements data fetching from a remote source using `useEffect` and manages the data using `useReducer`.
    
    Introduces a reducer function to handle different data states (loading, ready, error). This allows for a more structured and predictable way to manage the application's state, and prepare for the next steps.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import './css/App.css';
-import Main from './Main';
-import Header from './Header';
+import "./css/App.css";
+import Main from "./Main";
+import Header from "./Header";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
 
 function App() {
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+        case "dataFailed":
+          return {
+            ...state,
+            status: "error"
+          }
+      default:
+        throw new Error("Action unkonwn");
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({type: "dataFailed"}));
+  }, []);
+
   return (
     <div className="App">
       <Header />
